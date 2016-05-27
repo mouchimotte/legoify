ca08d1d1be0a76d33c576b5f735bc8c35f924e8e
Mouchi Motte <mouchi@mouchi.fr>
2016-05-28 01:51:51 +0200

    the script use the knowed pieces to redraw a new image (x10) with pieces og lego drawed

--- scripts/main.py
+++ scripts/main.py
@@ -12,6 +12,8 @@ pieces = {
     'pixel': 1
 }
 
+filename = "hi.png"
+
 def divide_by_piece(nb_contigus):
     mapping = {}
     for key in sorted(pieces):
@@ -23,7 +25,7 @@ def divide_by_piece(nb_contigus):
     return mapping
 
 if __name__ == "__main__":
-    img = Image.open("hi.png")
+    img = Image.open(filename)
     print('Image information:\n', img.format, img.size, img.mode)
     pixels = img.load()
     mapping = []
@@ -32,11 +34,9 @@ if __name__ == "__main__":
     #color stats
     for line in range(img.size[1]):
         for column in range(img.size[0]):
-            #debug print("ca > {0}".format(pixels[line, column]), "ca")
             if "{0}".format(pixels[column, line]) not in colors:
                 colors["{0}".format(pixels[column, line])] = []
             colors["{0}".format(pixels[column, line])].append([column, line])
-    #debug print(colors)
     print("Colors: ")
     for color in colors:
         pct = len(colors[color]) / (img.size[0] * img.size[1])
@@ -52,7 +52,31 @@ if __name__ == "__main__":
             color = pixels[column, line]
             if (column + 1) == img.size[0] \
                or color != pixels[column + 1, line]:
-                mapping[line].append({color: divide_by_piece(nb_contigus)})
+                mapping[line].append({'color': color,
+                                      'conf': divide_by_piece(nb_contigus)})
                 nb_contigus = 0
+    #for line in range(len(mapping)):
+    #    print("line {}> ".format(line + 1), mapping[line])
+
+    #build the image with pieces
+    img_dest = Image.new("RGB", (img.size[0] * 10, img.size[1] * 10))
     for line in range(len(mapping)):
-        print("line {}> ".format(line + 1), mapping[line])
+        column = 0
+        #work color block by color block
+        for color_block in mapping[line]:
+            #there are color and conf key to describe the block
+            #print(color_block['color'], color_block['conf'])
+            for piece in color_block['conf']:
+                for piece_inc in range(color_block['conf'][piece]):
+                    size = pieces[piece]
+                    line_dest = line * 10
+                    for line_inc in range(9):
+                        column_dest = column * 10
+                        for column_inc in range(size * 10 - 1):
+                            img_dest.putpixel([column_dest, line_dest],
+                                              color_block['color'])
+                            column_dest += 1
+                        line_dest += 1
+                    column += size
+        #quit()
+    img_dest.save("legoify_" + filename);
